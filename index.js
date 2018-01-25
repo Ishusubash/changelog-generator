92a334a1bb614409b539c033b1419aa93f00685f
Iswaryaa <sibiraj_s@live.com>
2018-01-25 14:07:04 +0530

    chore: update repo

--- index.js
+++ index.js
@@ -7,4 +7,8 @@ app.use(serveStatic(path.join(__dirname, 'src'), {
     'index': ['index.html', 'index.html']
 }))
 
+app.get('/generate-changelog', (req, res, next) => {
+    console.log(req);
+})
+
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
